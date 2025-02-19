8dcb83a1c1b7e6538bd9a10a1d5d712594d1058f
joaopedrosantanamotalol <jp1119174@gmail.com>
2025-02-19 14:30:39 -0300

    add e mudei a pagina próxima

--- resources/views/proxima.blade.php
+++ resources/views/proxima.blade.php
@@ -4,8 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"  rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
 </head>
 <body>
-    <h5>VOCÊ É INCRIVEL</h5>
+<div class="card" style=" display: flex; width: 18rem; aling-content:center; justify-content:center;">
+  <img src="https://i.pinimg.com/originals/72/4c/b7/724cb7b18dd9a1401ccaf6f8f9dcf7fc.jpg" class="card-img-top" alt="..." style="max-height:300px; max-width:300px;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
 </body>
 </html>
